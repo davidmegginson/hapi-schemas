28a52db7f1350acb4b879b121200bc41c5850179
turnerm <turnermon@gmail.com>
2023-08-28 10:16:04 -0400

    Changed dates to datetimes

--- sql/samples/op-data.sql
+++ sql/samples/op-data.sql
@@ -11,10 +11,10 @@
 
 INSERT INTO OperationalPresence (id, resource_ref, org_ref, sector_code, admin2_ref, reference_period_start, reference_period_end, source_data)
 VALUES
-(1, 1, 1, 'SHL', 2, '2023-01-01', NULL, 'DATA,DATA,DATA'),
-(2, 1, 2, 'FSC', 4, '2023-01-01', NULL, 'DATA,DATA,DATA'),
-(3, 1, 3, 'WSH', 4, '2023-01-01', NULL, 'DATA,DATA,DATA'),
-(4, 1, 3, 'HEA', 6, '2023-01-01', NULL, 'DATA,DATA,DATA'),
-(5, 1, 2, 'WSH', 1, '2023-01-01', NULL, 'DATA,DATA,DATA');
+(1, 1, 1, 'SHL', 2, '2023-01-01 00:00:00', NULL, 'DATA,DATA,DATA'),
+(2, 1, 2, 'FSC', 4, '2023-01-01 00:00:00', NULL, 'DATA,DATA,DATA'),
+(3, 1, 3, 'WSH', 4, '2023-01-01 00:00:00', NULL, 'DATA,DATA,DATA'),
+(4, 1, 3, 'HEA', 6, '2023-01-01 00:00:00', NULL, 'DATA,DATA,DATA'),
+(5, 1, 2, 'WSH', 1, '2023-01-01 00:00:00', NULL, 'DATA,DATA,DATA');
 
 -- end
--- sql/samples/pop-data.sql
+++ sql/samples/pop-data.sql
@@ -11,12 +11,12 @@
 
 INSERT INTO Population (id, resource_ref, admin2_ref, gender_code, age_range_code, population, reference_period_start, reference_period_end, source_data)
 VALUES
-(1, 1, 1, NULL, NULL, 1000000, '2023-01-01', '2023-06-30', 'DATA,DATA,DATA'),       -- total national
-(2, 1, 1, 'f', NULL, 500001, '2023-01-01',  '2023-06-30','DATA,DATA,DATA'),         -- national (f), all ages
-(3, 1, 1, 'm', NULL, 489999, '2023-01-01',  '2023-06-30','DATA,DATA,DATA'),         -- national (f), all ages
-(4, 1, 1, 'x', NULL, 9999, '2023-01-01',  '2023-06-30','DATA,DATA,DATA'),           -- national (x), all ages
-(5, 1, 1, 'f', '0-4', 300000, '2023-01-01',  '2023-06-30','DATA,DATA,DATA'),   -- national (f) children
-(6, 1, 2, 'x', '5-9', 2000, '2023-01-01',  '2023-06-30','DATA,DATA,DATA'),  -- admin1 (x) adolescents
-(7, 1, 4, 'm', '10-14', 100000, '2023-01-01',  '2023-06-30','DATA,DATA,DATA');    -- admin2 (m) elderly
+(1, 1, 1, NULL, NULL, 1000000, '2023-01-01 00:00:00', '2023-06-30 00:00:00', 'DATA,DATA,DATA'),       -- total national
+(2, 1, 1, 'f', NULL, 500001, '2023-01-01 00:00:00',  '2023-06-30 00:00:00','DATA,DATA,DATA'),         -- national (f), all ages
+(3, 1, 1, 'm', NULL, 489999, '2023-01-01 00:00:00',  '2023-06-30 00:00:00','DATA,DATA,DATA'),         -- national (f), all ages
+(4, 1, 1, 'x', NULL, 9999, '2023-01-01 00:00:00',  '2023-06-30 00:00:00','DATA,DATA,DATA'),           -- national (x), all ages
+(5, 1, 1, 'f', '0-4', 300000, '2023-01-01 00:00:00',  '2023-06-30 00:00:00','DATA,DATA,DATA'),   -- national (f) children
+(6, 1, 2, 'x', '5-9', 2000, '2023-01-01 00:00:00',  '2023-06-30 00:00:00','DATA,DATA,DATA'),  -- admin1 (x) adolescents
+(7, 1, 4, 'm', '10-14', 100000, '2023-01-01 00:00:00',  '2023-06-30 00:00:00','DATA,DATA,DATA');    -- admin2 (m) elderly
 
 -- end
